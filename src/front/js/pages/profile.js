3c173d7ac2623615fb5df8d27f89302f285fd5d7
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-03-31 22:12:46 +0000

    Styled the profile page, added in save options, linked the macrocalulator

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -41,44 +41,83 @@ const Private = () => {
 
   return (
     <div className="container text-center">
-      <Macrocalculator />
-      <h1>Hello!</h1>
-      {store.user && (
-        <div>
-          <h2>Email: {store.user.email}</h2>
+      <h2>Edit Profile:</h2>
+      <div style={{ display: "flex" }}>
+        {store.user && (
+          <div>
+            <h2>Email: {store.user.email}</h2>
+          </div>
+        )}
+        <div className="container-fluid" style={{ width: "30%", marginLeft: 0 }}>
+          <div className="row">
+            {/* Sidebar */}
+            <div className="col-md-3 sidebar">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">Macro Tracker</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Marco Calculator</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Upgrade</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#macro">Settings</a>
+                </li>
+                {/* Add more sidebar options as needed */}
+              </ul>
+            </div>
+          </div>
         </div>
-      )}
-      <div className="container-fluid">
-        <div className="row">
-          {/* Sidebar */}
-          <div className="col-md-3 sidebar">
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Macro Tracker</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Recipes</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Settings</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Upgrade</a>
-              </li>
-              {/* Add more sidebar options as needed */}
-            </ul>
+        <div style={{ height: "100dvh", width: "70%" }}>
+          <div style={{
+            display: "flex",
+            flexDirection: "column-reverse",
+          }}>
+            <input type="file" onChange={handleChange} />
+            <img src={file} height="200px" width="200px" style={{ borderRadius: "50%", border: "1px solid black" }} />
           </div>
-
-          <div className="col-md-9 main-content">
-            <h2>Profile</h2>
-            {/* Add upload picture field and input fields for user information */}
+          <div>
+            <form>
+              <div className="mb-3" style={{ width: "50%", paddingTop: "5px" }}>
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              </div>
+              <div className="mb-3" style={{ width: "50%" }}>
+                <label htmlFor="exampleInputPassword1" className="form-label">Name</label>
+                <input type="password" className="form-control" id="exampleInputName" />
+              </div>
+              <div className="mb-3" style={{ width: "50%" }}>
+                <label htmlFor="exampleInputPassword1" className="form-label">Weight</label>
+                <input type="password" className="form-control" id="exampleInputWeight" />
+              </div>
+              <div className="mb-3" style={{ width: "50%" }}>
+                <label htmlFor="exampleInputPassword1" className="form-label">Current Track</label>
+                <input type="password" className="form-control" id="exampleInputCurrentTrack" />
+              </div>
+              <div className="mb-3" style={{ width: "50%" }}>
+                <div class="btn-group">
+                <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dietary Preferences
+                </button>
+                <ul className="dropdown-menu">
+                    ...
+                </ul>
+                </div>
+              </div>
+<div className="btn-group">
+  <button className="btn btn-secondary btn-sm" type="button"></button>
+              </div>
+              <button type="submit" className="btn btn-primary">Save</button>
+            </form>
           </div>
-
         </div>
       </div>
-      <input type="file" onChange={handleChange} />
-      <img src={file} height="300px" width="300px" />
-    </div>
+
+
+      <Macrocalculator />
+    </div >
   );
 };
 
