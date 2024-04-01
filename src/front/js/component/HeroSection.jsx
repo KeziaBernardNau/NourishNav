288cabd37d79c1d0780383383dbb763dbd1eeb87
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-04-01 23:42:34 +0000

    created a new div to house logo on the homepage

--- src/front/js/component/HeroSection.jsx
+++ src/front/js/component/HeroSection.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import '../../styles/hero.css';
+import React from "react";
+import "../../styles/hero.css";
+import nourishNav from "../../img/assets/nourishNav.gif";
 
 const HeroSection = () => {
   return (
@@ -7,14 +8,20 @@ const HeroSection = () => {
       <div className="hero-background"></div>
       <div className="hero-overlay"></div>
       <div className="hero-content">
-        <h1 className='banner-title'>Discover Healthier Living with NourishNav</h1>
-        <p className='banner-text'>Track Your Macros, Elevate Your Health! </p>
-        <a href="#about" className="btn">Learn More</a>
+        <h1 className="banner-title">
+          Discover Healthier Living with NourishNav
+        </h1>
+        <p className="banner-text">Track Your Macros, Elevate Your Health!</p>
+        <a href="#about" className="btn">
+          Learn More
+        </a>
+      </div>
+      {/* container for nourishNav logo */}
+      <div className="hero-gif">
+        <img src={nourishNav} alt="nourishNav Logo" />
       </div>
     </div>
   );
 };
 
 export default HeroSection;
-
-
