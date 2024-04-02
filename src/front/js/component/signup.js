1cedda04dcb9b842610fa0025e9cb7e5e5829b9a
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-04-02 00:24:37 +0000

    styled the signup page, added image

--- src/front/js/component/signup.js
+++ src/front/js/component/signup.js
@@ -27,44 +27,48 @@ export default function Signup() {
     };
 
     return (
-        <div className="container">
-            <h3>Join Us</h3>
-            <p>Welcome! Join us by signing up below.</p>
-            {/* Input fields */}
-            <div className="input-group">
-                <p>Email</p>
-                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <div className="signup"> 
+            <div className="sign-background"></div> 
+            <div className="signup-overlay"></div> 
+            <div className="sign-content"> 
+                <h3 className="banner-title">Join Us</h3> 
+                <p>Welcome! Join us by signing up below.</p>
+                
+                <div className="input-group">
+                    <p>Email</p>
+                    <input type="email" className="input-field" onChange={(e) => setEmail(e.target.value)} value={email} />
+                </div>
+                <div className="input-group">
+                    <p>Password</p>
+                    <input type="password" className="input-field" onChange={(e) => setPassword(e.target.value)} value={password} />
+                </div>
+                <div className="input-group">
+                    <p>Age</p>
+                    <input type="number" className="input-field" onChange={(e) => setAge(e.target.value)} value={age} />
+                </div>
+                <div className="input-group">
+                    <p>Height (in cm)</p>
+                    <input type="number" className="input-field" onChange={(e) => setHeight(e.target.value)} value={height} />
+                </div>
+                <div className="input-group">
+                    <p>Weight (in kg)</p>
+                    <input type="number" className="input-field" onChange={(e) => setWeight(e.target.value)} value={weight} />
+                </div>
+                <div className="input-group">
+                    <p>Activity Level:</p>
+                    <select className="input-field" value={activity} onChange={(e) => setActivity(e.target.value)}>
+                        <option value="Very Active">Very Active</option>
+                        <option value="Less">Less</option>
+                        <option value="None">None</option>
+                        <option value="Disabled">Disabled</option>
+                    </select>
+                </div>
+                {loginError && <p className="error-message">{loginError}</p>}
+                <div className="button-group">
+                    <button className="btn" onClick={handleSignup}>Sign up</button>
+                </div>
+                <p>Already a member? <span className="text-primary" onClick={() => navigate('/login')}>Log in</span></p>
             </div>
-            <div className="input-group">
-                <p>Password</p>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-            </div>
-            <div className="input-group">
-                <p>Age</p>
-                <input type="number" onChange={(e) => setAge(e.target.value)} value={age} />
-            </div>
-            <div className="input-group">
-                <p>Height (in cm)</p>
-                <input type="number" onChange={(e) => setHeight(e.target.value)} value={height} />
-            </div>
-            <div className="input-group">
-                <p>Weight (in kg)</p>
-                <input type="number" onChange={(e) => setWeight(e.target.value)} value={weight} />
-            </div>
-            <div className="input-group">
-                <p>Activity Level:</p>
-                <select value={activity} onChange={(e) => setActivity(e.target.value)}>
-                    <option value="Very Active">Very Active</option>
-                    <option value="Less">Less</option>
-                    <option value="None">None</option>
-                    <option value="Disabled">Disabled</option>
-                </select>
-            </div>
-            {loginError && <p className="text-danger">{loginError}</p>}
-            <div className="button-group">
-                <button onClick={handleSignup}>Sign up</button>
-            </div>
-            <p>Already a member? <span className="text-primary" onClick={() => navigate('/login')}>Log in</span></p>
         </div>
     );
 };
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,57 +1,7 @@
-// import React, { useState, useContext } from "react";
-// import "../../styles/loginpage.css"; // Import CSS file
-// import { Context } from "../store/appContext";
-// import { useNavigate, Link } from "react-router-dom";
-
-// export default function Login() {  
-//   const { store, actions } = useContext(Context);
-//   const [email, setemail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState(null);
-//   const navigate = useNavigate();
-
-//   const token = sessionStorage.getItem("token");
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//         await actions.login({ email, password });
-//         navigate('/profile'); 
-//     } catch (error) {
-//         setError(error.message);
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={handleSubmit}>
-//         <h1 className="">Login</h1>
-//         <input
-//           type="text"
-//           placeholder="email"
-//           value={email}
-//           onChange={(e) => setemail(e.target.value)}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         {error && <p className="error-message">{error}</p>}
-//         <button type="submit">Login</button>
-//         <div className="link-button">
-//           <Link to="/forgot">Forgot Password</Link>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// }
-
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate, Link } from "react-router-dom";
-import "../../styles/loginpage.css"; // Import CSS file
+import "../../styles/loginpage.css"; 
 
 export default function Login() {  
   const { store, actions } = useContext(Context);
