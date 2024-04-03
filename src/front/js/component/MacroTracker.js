1f91946a62627775aee68b2ea1baca03ac937ac2
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-04-03 00:09:55 +0000

    refactored the macro page

--- src/front/js/component/MacroTracker.js
+++ src/front/js/component/MacroTracker.js
@@ -1,199 +1,7 @@
-// import React, { useState, useEffect } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "../../styles/macroTracker.css";
-// import { Macrotrackerapi } from "../component/macrotrackerapi"; // Assuming correct path
-
-// function MacroTracker() {
-//   const [meals, setMeals] = useState({
-//     breakfast: [],
-//     lunch: [],
-//     dinner: [],
-//     snack: [],
-//   });
-//   const [totalMacros, setTotalMacros] = useState({
-//     calories: 0,
-//     protein: 0,
-//     fat: 0,
-//     carbohydrates: 0,
-//   });
-//   const [eatenCalories, setEatenCalories] = useState(0);
-//   const [caloriesBurned, setCaloriesBurned] = useState(0);
-//   const totalCaloriesAllowed = 2000; // Total calories allowed per day
-//   const [circlePosition, setCirclePosition] = useState(0);
-//   const [selectedDate, setSelectedDate] = useState(() => {
-//     const today = new Date();
-//     const localDate = today.toLocaleDateString("en-CA"); // YYYY-MM-DD format
-//     return localDate;
-//   });
-//   const [showModal, setShowModal] = useState(false);
-//   const [mealTypeToAdd, setMealTypeToAdd] = useState("");
-//   const [foodItem, setFoodItem] = useState("");
-//   const [calories, setCalories] = useState("");
-//   const [protein, setProtein] = useState("");
-//   const [fat, setFat] = useState("");
-//   const [carbohydrates, setCarbohydrates] = useState("");
-
-//   useEffect(() => {
-//     document.body.style.background =
-//       "linear-gradient(to bottom right, #72bb53, #ffffff)";
-
-//     return () => {
-//       document.body.style.background = "";
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     const calculateCirclePosition = () => {
-//       const percentConsumed = (eatenCalories / totalCaloriesAllowed) * 100;
-//       setCirclePosition(percentConsumed);
-//     };
-
-//     calculateCirclePosition();
-//   }, [eatenCalories]);
-
-//   const addFood = () => {
-//     const newFoodItem = {
-//       id: Date.now(), // Unique ID for each food item
-//       foodItem,
-//       calories: parseInt(calories),
-//       protein: parseInt(protein),
-//       fat: parseInt(fat),
-//       carbohydrates: parseInt(carbohydrates),
-//     };
-
-//     setMeals((prevMeals) => ({
-//       ...prevMeals,
-//       [mealTypeToAdd]: [...prevMeals[mealTypeToAdd], newFoodItem],
-//     }));
-//     console.log(totalMacros);
-//     console.log(parseInt(calories));
-//     console.log(parseInt(protein));
-//     setTotalMacros((prevTotalMacros) => ({
-//       calories: prevTotalMacros.calories + parseInt(calories),
-//       protein: prevTotalMacros.protein + parseInt(protein),
-//       fat: prevTotalMacros.fat + parseInt(fat),
-//       carbohydrates: prevTotalMacros.carbohydrates + parseInt(carbohydrates),
-//     }));
-
-//     setEatenCalories(
-//       (prevEatenCalories) => prevEatenCalories + parseInt(calories)
-//     );
-
-//     setFoodItem("");
-//     setCalories("");
-//     setProtein("");
-//     setFat("");
-//     setCarbohydrates("");
-//     setShowModal(false);
-//   };
-
-//   const deleteFood = (mealType, id, macros) => {
-//     const updatedMeals = {
-//       ...meals,
-//       [mealType]: meals[mealType].filter((item) => item.id !== id),
-//     };
-//     setMeals(updatedMeals);
-
-//     setTotalMacros((prevTotalMacros) => ({
-//       calories: prevTotalMacros.calories - macros.calories,
-//       protein: prevTotalMacros.protein - macros.protein,
-//       fat: prevTotalMacros.fat - macros.fat,
-//       carbohydrates: prevTotalMacros.carbohydrates - macros.carbohydrates,
-//     }));
-
-//     setEatenCalories(
-//       (prevEatenCalories) => prevEatenCalories - macros.calories
-//     );
-//   };
-
-//   const handleDateChange = (e) => {
-//     setSelectedDate(e.target.value);
-//   };
-
-//   const handleAddMeal = (mealType) => {
-//     setMealTypeToAdd(mealType);
-//     setShowModal(true);
-//   };
-
-//   // Function to calculate recommended calories for each meal type
-//   const getRecommendedCalories = (mealType) => {
-//     switch (mealType) {
-//       case "breakfast":
-//         return 500; // 25% of 2000 calories
-//       case "lunch":
-//       case "dinner":
-//         return 600; // 30% of 2000 calories
-//       case "snack":
-//         return 300; // 15% of 2000 calories
-//       default:
-//         return 0;
-//     }
-//   };
-
-//   // Function to get the appropriate Font Awesome icon for each meal type
-//   const getMealIcon = (mealType) => {
-//     switch (mealType) {
-//       case "breakfast":
-//         return <i className="fa-solid fa-mug-saucer"></i>;
-//       case "lunch":
-//         return <i className="fa-solid fa-hotdog"></i>;
-//       case "dinner":
-//         return <i className="fa-solid fa-bowl-food"></i>;
-//       case "snack":
-//         return <i className="fa-solid fa-apple-whole"></i>;
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <div className="macro-container">
-//       <div className="macro-api-section">
-//         <Macrotrackerapi onAdd={addFood} />
-//       </div>
-//       <div className="food-entries-section">
-//         {Object.entries(meals).map(([mealType, items]) => (
-//           <div key={mealType}>
-//             <h2>{mealType.charAt(0).toUpperCase() + mealType.slice(1)}</h2>
-//             <ul>
-//               {items.map((item) => (
-//                 <li key={item.id}>
-//                   {item.foodItem} - {item.calories} calories, {item.protein}g protein,
-//                   {item.fat}g fat, {item.carbohydrates}g carbohydrates
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         ))}
-//         <div className="total-macros">
-//           <h3>Total Macros</h3>
-//           <p>Calories: {totalMacros.calories}</p>
-//           <p>Protein: {totalMacros.protein}g</p>
-//           <p>Fat: {totalMacros.fat}g</p>
-//           <p>Carbohydrates: {totalMacros.carbohydrates}g</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default MacroTracker;
-
-
-
-
-
-
-
-
-
-
-
-// original code with api
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../../styles/macroTracker.css";
-import { Macrotrackerapi } from "../component/macrotrackerapi"; // Ensure this import matches the export
+import "../../styles/macroTracker.css"; // Ensure this path is correct
+import { Macrotrackerapi } from "../component/macrotrackerapi";
 
 function MacroTracker() {
   const [meals, setMeals] = useState({
@@ -208,16 +16,8 @@ function MacroTracker() {
     fat: 0,
     carbohydrates: 0,
   });
+  const [selectedMealType, setSelectedMealType] = useState("");
 
-  useEffect(() => {
-    document.body.style.background =
-      "linear-gradient(to bottom right, #72bb53, #ffffff)";
-    return () => {
-      document.body.style.background = "";
-    };
-  }, []);
-
-  // Modified function to accept mealType
   const handleAddFood = (nutrition, mealType) => {
     const newFoodItem = {
       id: Date.now(),
@@ -230,10 +30,9 @@ function MacroTracker() {
 
     setMeals((prevMeals) => ({
       ...prevMeals,
-      [mealType]: [...prevMeals[mealType], newFoodItem], // Add to specific meal type
+      [mealType]: [...prevMeals[mealType], newFoodItem],
     }));
 
-    // Update total macros
     setTotalMacros((prev) => ({
       calories: prev.calories + newFoodItem.calories,
       protein: prev.protein + newFoodItem.protein,
@@ -242,34 +41,52 @@ function MacroTracker() {
     }));
   };
 
+  const toggleMealType = (mealType) => {
+    setSelectedMealType(selectedMealType === mealType ? "" : mealType);
+  };
+
   return (
     <div className="macro-container">
-      <h1 style={{ color: "#FFFFFF", textAlign: "center", fontWeight: "bold" }}>
-        NourishNav
-      </h1>
+      <h1 className="title">NourishNav</h1>
 
-      <Macrotrackerapi onAdd={handleAddFood} />
-
-      {Object.entries(meals).map(([mealType, items]) => (
-        <div key={mealType}>
-          <h2>{mealType.charAt(0).toUpperCase() + mealType.slice(1)}</h2>
-          <ul>
-            {items.map((item) => (
-              <li key={item.id}>
-                {item.foodItem} - {item.calories} calories, {item.protein}g protein,
-                {item.fat}g fat, {item.carbohydrates}g carbohydrates
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-
-      <div className="total-macros">
+      <div className="total-macros section">
         <h3>Total Macros</h3>
         <p>Calories: {totalMacros.calories}</p>
         <p>Protein: {totalMacros.protein}g</p>
         <p>Fat: {totalMacros.fat}g</p>
         <p>Carbohydrates: {totalMacros.carbohydrates}g</p>
+      </div>
+
+      <div className="flex-container">
+        <div className="macro-api-section section">
+          <Macrotrackerapi onAdd={handleAddFood} />
+        </div>
+
+        <div className="food-entries-section section">
+          {["breakfast", "lunch", "dinner", "snack"].map((mealType) => (
+            <div key={mealType} className="meal-type-row">
+              <button
+                onClick={() => toggleMealType(mealType)}
+                className="meal-type-button"
+              >
+                {mealType.charAt(0).toUpperCase() + mealType.slice(1)}
+              </button>
+              {selectedMealType === mealType && (
+                <div className="meal-details-container">
+                  <ul className="list-group no-bullets">
+                    {meals[mealType].map((item) => (
+                      <li key={item.id} className="list-group-item">
+                        {item.foodItem} - {item.calories} calories,{" "}
+                        {item.protein}g protein, {item.fat}g fat,{" "}
+                        {item.carbohydrates}g carbohydrates
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
